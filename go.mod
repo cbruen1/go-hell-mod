204480d772279dca4e6065f5eb46d0388807a3d3
Ciaran <303601+cbruen1@users.noreply.github.com>
2020-03-02 23:19:57 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cbruen1/go-hell-mod
+
+go 1.14
---
+++ hello.go
@@ -0,0 +1,25 @@
+package package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", nil)
+}
+
+import (
+	"fmt"
+)
+
+func SayHello() {
+	fmt.Println("Hello Go world :)")
+	// js.Global().Call("alert", "Hello World")
+}
